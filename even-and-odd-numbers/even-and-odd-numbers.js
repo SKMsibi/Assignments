8443643151921a0bfaa1e067f847e838a3904ca8
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-09-15 16:17:32 +0000

    asigned the variables numbers,evenOrOdd to 0 and results to an empty string

--- even-and-odd-numbers/even-and-odd-numbers.js
+++ even-and-odd-numbers/even-and-odd-numbers.js
@@ -1,14 +1,14 @@
 var maxNumber = 50;
 function evaluateNumbers(maxNumber) {
-    var numbers;
-    var evenOrOdd;
-    var results;
+    var numbers = 0;
+    var evenOrOdd = 0;
+    var results = " ";
     var i = 1;
     while (i <= maxNumber){
         numbers = i;
-    if (i % 2 == 0 && i > 1) {
+    if(i % 2 === 0 && i > 1) {
         evenOrOdd = "this is an even";
-    } else {
+    } else{
         evenOrOdd = "this is an odd"
     }
     results += `${numbers} ${evenOrOdd} "\n"`;
@@ -16,4 +16,4 @@ function evaluateNumbers(maxNumber) {
 }
 return results;
 }
-console.log(evaluateNumbers(10));
+console.log(evaluateNumbers(maxNumber));
