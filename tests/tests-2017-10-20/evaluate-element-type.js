e35709baebc1684cf35ea8fec5b2812617c5f21f
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-10-20 10:36:18 +0000

    created a function that evaluates elements of a list and detirmines wether they are numbers of not

--- tests/tests-2017-10-20/evaluate-element-type.js
+++ tests/tests-2017-10-20/evaluate-element-type.js
@@ -1,6 +1,24 @@
-function numbers(collection){
-    collection = Array.from(arguments);
-    return collection;
+function numbers(collection) {
+    var eachElementStatus = "";
+    var finalAnswer = "";
+    collection = Array.from(arguments)
+
+    for (var i = 0; i < collection.length; i++) {
+        if (isNaN(collection[i])) {
+            eachElementStatus += false;
+        } else {
+            eachElementStatus += true; 
+        }
+    }
+    if(eachElementStatus.search(/false/gi) >= 0){
+        finalAnswer = false;
+    }else{
+        finalAnswer = true;
+    }
+    return finalAnswer;
 }
 
-console.log(numbers("a","e",3,5,6,2));
+console.log(numbers(4, 3,"ad", 3, 4, 4, 2));
+console.log(numbers(4,6,22,NaN,56));
+console.log(numbers("Officer","Soldier"));
+console.log(numbers(1,4,6,3,7,8));
