e20a6789dcb165751d084b6fe59d151c66589f62
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-10-20 09:33:12 +0000

    created a function in the tests folder that returns the last element of a list

--- tests/tests-2017-10-20/retrieve-last-element.js
+++ tests/tests-2017-10-20/retrieve-last-element.js
@@ -1,6 +1,8 @@
 function retrieveLastElement(list){
     list = Array.from(arguments);
-    return list;
+    finalAnswer = list[list.length - 1];
+    return finalAnswer;
 }
 
 console.log(retrieveLastElement(5,4,7,3,6,32));
+console.log(retrieveLastElement("Earth", "Moon", "Jupiter", "Pluto"));
