af108371134aec9dd06b7dafe8f2f9bf8ef7d6c0
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-10-26 09:54:41 +0000

    added test scenarios to test the function

--- tests/tests-2017-10-26/return-less-than.js
+++ tests/tests-2017-10-26/return-less-than.js
@@ -8,3 +8,6 @@ function lessThan(list,givenNumber){
     return finalAnswer;
 }
 
+console.log(lessThan([1,2,4,6,11,78], 10));
+console.log(lessThan([12,45,75,4,7,86], 20));
+console.log(lessThan([3,11,5,76,34,5,3,54], 15));
