c508db85125fd8559bb549929048e2bfabb09f43
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-03 11:28:19 +0000

    changed the method i used to retrieve data

--- tests/tests-2017-11-03/duplify-string.js
+++ tests/tests-2017-11-03/duplify-string.js
@@ -1,8 +1,8 @@
-function duplifyString(input, dupNum){
+String.prototype.duplify = function(num){
     var element = "";
-    for (var index = 0; index < dupNum; index++) {
-        element += input;
+    for (var index = 0; index < num; index++) {
+        element += this;
     }
-return element;
+    return element;
 }
-console.log(duplifyString("Johannesburg", 3));
+console.log("Johannesburg".duplify(3));
