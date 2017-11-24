20754486ec8f3bc0fca0e9ea6b3b24b5103d7cc2
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-24 11:08:20 +0000

    created a function that returns the passed in arguments

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -1,20 +1,5 @@
 function intAdd(stringNumbers) {
-    var finalAnswer = 0;
-    switch (stringNumbers.length) {
-        case 0:
-            finalAnswer = stringNumbers.length;
-            break;
-        case 1:
-            finalAnswer = parseInt(stringNumbers);
-            break;
-        case 3:
-            var split = stringNumbers.split(",");
-            finalAnswer = split.reduce(function (a, b) {
-                return parseInt(a) + parseInt(b);
-            });
-            break;
-    }
-    return finalAnswer;
+return stringNumbers;
 }
 
 console.log(intAdd("1"));
