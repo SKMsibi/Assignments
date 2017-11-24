b62619c98fccc75c29f41d82f5c462028ef91d6e
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-24 11:10:15 +0000

    created a switch statement that returned returned 0 if an empty string was passed and return 1 if 1 was passed

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -1,5 +1,15 @@
 function intAdd(stringNumbers) {
-return stringNumbers;
+    var finalAnswer = 0;
+    switch (stringNumbers.length) {
+        case 0:
+            finalAnswer = stringNumbers.length;
+            break;
+        case 1:
+            finalAnswer = parseInt(stringNumbers);
+            break;
+
+    }
+    return finalAnswer;
 }
 
 console.log(intAdd("1"));
