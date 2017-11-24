4c80930744d662064272fdfd816d8f622577ae45
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-24 11:05:47 +0000

    added an if statement that returns 0 if an empty array was passed in

--- tests/test-2017-11-24/string-calculater-task-2.js
+++ tests/test-2017-11-24/string-calculater-task-2.js
@@ -1,4 +1,11 @@
-function intAdd(stringNumbers){
-    return stringNumbers;
+function intAdd(stringNumbers) {
+    var finalAnswer = 0;
+    if (stringNumbers.length === 0) {
+        return  0;
+    } 
+    return finalAnswer;
 }
-console.log(intAdd("1,3,5,6"));
+console.log(intAdd("1"));
+console.log(intAdd("1,3,6,3,2"));
+console.log(intAdd(""));
+
--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -16,7 +16,8 @@ function intAdd(stringNumbers) {
     }
     return finalAnswer;
 }
-console.log(intAdd(""));
+
 console.log(intAdd("1"));
+console.log(intAdd(""));
 console.log(intAdd("1,2"));
 
