698997a6ab8909078afe3f77b036ecf446ec4f2c
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-24 11:07:02 +0000

    added a else if statement that used a reduce function to add all string numbers if any numbers were passed in the string

--- tests/test-2017-11-24/string-calculater-task-2.js
+++ tests/test-2017-11-24/string-calculater-task-2.js
@@ -2,7 +2,13 @@ function intAdd(stringNumbers) {
     var finalAnswer = 0;
     if (stringNumbers.length === 0) {
         return  0;
-    } 
+    } else if (stringNumbers.length > 0) {
+        var sp = stringNumbers.split(",");
+
+        return sp.reduce(function (a, b) {
+            return parseInt(a) + parseInt(b);
+        });
+    }
     return finalAnswer;
 }
 console.log(intAdd("1"));
