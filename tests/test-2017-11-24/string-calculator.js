1dbc391922231d5ddf42b147daa0f8170a867e3e
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-29 10:23:01 +0000

    added another function that only dealt with delimiters

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -1,4 +1,15 @@
 function intAdd(stringnumbers) {
+    if (stringnumbers.indexOf(",\n") !== -1) {
+        return "invalid syntax";
+    }
+    else if (stringnumbers.indexOf("//") === -1) {
+        var split = stringnumbers.split(/[,\n]/g);
+        return split.reduce(function (a, b) { return parseFloat(a) + parseFloat(b) });
+    }
+    return workWithDelimiter(stringnumbers);
+}
+
+function workWithDelimiter(stringnumbers) {
     var splitString = stringnumbers.split("\n");
     var split = splitString[1].split(splitString[0][2]);
     var allPositive = split.every((element) => element >= 0);
@@ -10,8 +21,11 @@ function intAdd(stringnumbers) {
     var filtered = split.filter((element) => element < 0);
     throw new Error(`negatives are not allowed ${filtered}`);
 }
+
+console.log(intAdd("1,2\n3"));
+console.log(intAdd("1,2,\n3"));
 console.log(intAdd("//?\n1?2?3"));
 console.log(intAdd("//?\n"));
-console.log(intAdd("//,\n1,2,3"));
-console.log(intAdd("//,\n2.2,2.2"));
+console.log(intAdd("//;\n1;2;3"));
+console.log(intAdd("//#\n2.2#2.2"));
 console.log(intAdd("//:\n1:2:3:-4:-2"));
