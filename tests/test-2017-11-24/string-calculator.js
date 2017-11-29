f482635a5998e3901d87a81acc75bddf45936bbd
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-29 09:39:56 +0000

    added more arrow functions

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -1,13 +1,13 @@
 function intAdd(stringnumbers) {
     var splitString = stringnumbers.split("\n");
     var split = splitString[1].split(splitString[0][2]);
-    var allPositive = split.every(function (element) { return element >= 0 });
+    var allPositive = split.every((element) => element >= 0);
 
     while (allPositive) {
         var finalAnswer = split.reduce((a, b) => parseInt(a) + parseInt(b));
         return finalAnswer !== "" ? finalAnswer : 0;
     }
-    var filtered = split.filter(function (element) { return element < 0; });
+    var filtered = split.filter((element) => element < 0);
     throw new Error(`negatives are not allowed ${filtered}`);
 }
 // console.log(intAdd("//:\n1:2:3:-4:-2"));
