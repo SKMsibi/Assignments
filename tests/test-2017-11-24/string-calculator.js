294f1908da81cb6347092e3bdf47ddc2dcee4b8b
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-29 09:45:02 +0000

    enabled the string calculator to work for decimal numbers

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -4,13 +4,14 @@ function intAdd(stringnumbers) {
     var allPositive = split.every((element) => element >= 0);
 
     while (allPositive) {
-        var finalAnswer = split.reduce((a, b) => parseInt(a) + parseInt(b));
+        var finalAnswer = split.reduce((a, b) => parseFloat(a) + parseFloat(b));
         return finalAnswer !== "" ? finalAnswer : 0;
     }
     var filtered = split.filter((element) => element < 0);
     throw new Error(`negatives are not allowed ${filtered}`);
 }
-// console.log(intAdd("//:\n1:2:3:-4:-2"));
 console.log(intAdd("//?\n1?2?3"));
 console.log(intAdd("//?\n"));
 console.log(intAdd("//,\n1,2,3"));
+console.log(intAdd("//,\n2.2,2.2"));
+console.log(intAdd("//:\n1:2:3:-4:-2"));
