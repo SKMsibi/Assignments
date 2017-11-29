1a6600d7147602bd9d484530047b7594ed921fbc
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-11-29 10:38:53 +0000

    added more test cases

--- tests/test-2017-11-24/string-calculator.js
+++ tests/test-2017-11-24/string-calculator.js
@@ -1,5 +1,8 @@
 function intAdd(stringnumbers) {
-    if (stringnumbers.indexOf(",\n") !== -1) {
+    if(stringnumbers === ""){
+        return 0;
+    }
+    else if (stringnumbers.indexOf(",\n") !== -1) {
         return "invalid syntax";
     }
     else if (stringnumbers.indexOf("//") === -1) {
@@ -13,7 +16,6 @@ function workWithDelimiter(stringnumbers) {
     var splitString = stringnumbers.split("\n");
     var split = splitString[1].split(splitString[0][2]);
     var allPositive = split.every((element) => element >= 0);
-
     while (allPositive) {
         var finalAnswer = split.reduce((a, b) => parseFloat(a) + parseFloat(b));
         return finalAnswer !== "" ? finalAnswer : 0;
@@ -22,10 +24,11 @@ function workWithDelimiter(stringnumbers) {
     throw new Error(`negatives are not allowed ${filtered}`);
 }
 
+console.log(intAdd(""));
 console.log(intAdd("1,2\n3"));
 console.log(intAdd("1,2,\n3"));
 console.log(intAdd("//?\n1?2?3"));
 console.log(intAdd("//?\n"));
 console.log(intAdd("//;\n1;2;3"));
-console.log(intAdd("//#\n2.2#2.2"));
+console.log(intAdd("//-\n2.2-2.2-2.2"));
 console.log(intAdd("//:\n1:2:3:-4:-2"));
