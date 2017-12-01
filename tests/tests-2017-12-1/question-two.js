ed8f6b6a052f30cf7211dc8e814307c4dc788c72
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-12-01 09:56:21 +0000

    added an else if statement that caterd for arguments that are numbers but not greater then 0

--- tests/tests-2017-12-1/question-two.js
+++ tests/tests-2017-12-1/question-two.js
@@ -1,6 +1,8 @@
 function numberChecker(int){
     if(typeof int === "number" && int > 0){
         return "And";
+    }else if(typeof int === "number" && int < 0){
+        return "Or";
     }
 }
 console.log(numberChecker(1));
