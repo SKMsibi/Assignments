e2ab3634bd00b2778c50a7fc32b655fc5a12e3fa
Sabelo <sabelokabelomsibiskm@gmail.com>
2017-12-01 10:51:22 +0000

    added a else if statement that checks for less then numbers

--- tests/tests-2017-12-1/question-four.js
+++ tests/tests-2017-12-1/question-four.js
@@ -1,7 +1,9 @@
 function arrayFilter(arr, num, str) {
     if (str === "GreaterOrEqual") {
         return arr.filter((element) => element >= num);
+    } else if (str === "LessThan") {
+        return arr.filter((element) => element < num);
     }
 }
-console.log(arrayFilter([1,2,3,4,5,6,7], 3, "GreaterOrEqual"));
-console.log(arrayFilter([1,2,3,4,5,6,7], 5, "LessThan"));
+console.log(arrayFilter([1, 2, 3, 4, 5, 6, 7], 3, "GreaterOrEqual"));
+console.log(arrayFilter([1, 2, 3, 4, 5, 6, 7], 5, "LessThan"));
